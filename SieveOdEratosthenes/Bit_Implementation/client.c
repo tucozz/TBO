da42b1aa1a33352ecc4768c05a65ab7c71a8508e
Tucozz <tutuestefanato@gmail.com>
2023-08-18 16:02:05 -0300

    Bit Implementation

---
+++ SieveOdEratosthenes/Bit_Implementation/client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "sieve.h"
+
+int main(){
+    int N = 0;
+
+    scanf("%d", &N);
+
+    FindAllPrimes(N);
+
+    return 0;
+}
---
+++ SieveOdEratosthenes/Bit_Implementation/sieve.h
@@ -0,0 +1,6 @@
+//creates a list between 2 and N filled with 1s;
+int *CreateList(int N);
+//Marks as 0 all numbers that are multiples of X;
+void MarkNonPrimes(int *List, int X, int N);
+//find all primes up until N and return the array;
+void FindAllPrimes(int N);
