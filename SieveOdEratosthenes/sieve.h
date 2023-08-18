36da8391d4215561c483fe951da5f52ef341c2bf
Tucozz <tutuestefanato@gmail.com>
2023-08-18 15:25:50 -0300

    Question 1

---
+++ SieveOdEratosthenes/sieve.h
@@ -0,0 +1,7 @@
+
+//creates a list between 2 and N filled with 1s;
+int *CreateList(int N);
+//Marks as 0 all numbers that are multiples of X;
+void MarkNonPrimes(int *List, int X);
+//find all primes up until N and return the array;
+void FindAllPrimes(int N);
