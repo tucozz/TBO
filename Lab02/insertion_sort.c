d1c0c686acdd302b2c220c97bba1755c0d2378bf
Tucozz <tutuestefanato@gmail.com>
2023-08-29 16:43:36 -0300

    insertion implementation

---
+++ Lab02/insertion_sort.c
@@ -0,0 +1,10 @@
+#include "item.h"
+
+void sort(Item *a, int lo, int hi){
+    for(int i = lo; i < hi; i++){
+        for(int j = i-1; j >= lo; j--){
+            if(less(a[j+1], a[j]))
+                exch(a[j], a[j+1]);
+        }
+    }
+}
