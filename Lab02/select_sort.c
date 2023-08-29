272637e4367b9f7603ab2dfc17a8cab438010211
Tucozz <tutuestefanato@gmail.com>
2023-08-29 16:27:14 -0300

    select sort full

--- Lab02/select_sort.c
+++ Lab02/select_sort.c
@@ -1,12 +1,16 @@
 #include "item.h"
+#include "stdio.h"
 
 void sort(Item *a, int lo, int hi){
+    //escolhe qual o idx que a gente vai ACERTAR agora
     for(int i = lo; i < hi; i++){
-        int MIN = lo;
-        for(int j = lo + i; j < hi; j++){
+        //fala "0k. a partir de i, qual o menor numero?"
+        Item MIN = i;
+        for(int j = i + 1; j < hi; j++){
             if(less(a[j], a[MIN]))
                 MIN = j;
         }
+        //decidido o menor numero, troca ele de lugar com o atual
         exch(a[i], a[MIN]);
     }
 }
