aadc37ca693a6234897d381aebd6cf50cf24dbaa
Tucozz <tutuestefanato@gmail.com>
2023-09-26 16:33:56 -0300

    versao_2 MACRO

--- Lab05/quick_sort.c
+++ Lab05/quick_sort.c
@@ -1,4 +1,7 @@
 #include "item.h"
+//#if defined VERSAO_3 || VERSAO_4
+#include <stdlib.h>
+//#endif
 
 int partition(Item *a, int lo, int hi){
     int i = lo, j = hi+1;
@@ -15,9 +18,40 @@ int partition(Item *a, int lo, int hi){
     return j; // retorna o idx do nucleo que COM CERTEZA ta no lugar certo
 }
 
-void quick_sort(Item *a, int lo, int hi){
-    if(hi <= lo) return;
+#if defined VERSAO_2 || defined VERSAO_4
 
+void insertion_sort(Item *a, int lo, int hi){
+    for(int i = lo; i <= hi; i++){
+        for(int j = i-1; j >= lo; j--){
+            if(less(a[j+1], a[j]))
+                exch(a[j], a[j+1]);
+        }
+    }
+}
+
+#endif
+
+//#if defined VERSAO_3 || defined VERSAO_4
+    
+int median_of_3(Item *a, int lo, int hi){
+
+}
+
+//#endif
+
+void quick_sort(Item *a, int lo, int hi){
+    #ifdef CUTOFF
+    if(hi <= lo + CUTOFF - 1){
+        insertion_sort(a, lo, hi);
+    #else
+    if(hi <= lo){
+    #endif
+        return;
+    }
+    #if defined VERSAO_3 || defined VERSAO_4
+    int median = median_of_3(a, lo, hi);
+    exch(a[lo], a[median]);
+    #endif
     int mid = partition(a, lo, hi);
     quick_sort(a, lo, mid-1);
     quick_sort(a, mid+1, hi);
