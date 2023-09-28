45f898dfbdce9354b9852684e36d6fbf208af3b2
Tucozz <tutuestefanato@gmail.com>
2023-09-28 14:12:28 -0300

    merge_sort versions

--- Algorithms/merge_sort.c
+++ Algorithms/merge_sort.c
@@ -1,4 +1,18 @@
 #include "item.h"
+#include <stdlib.h>
+
+#if defined VERSAO_2 || defined VERSAO_4
+
+void insertion_sort(Item *a, int lo, int hi){
+    for(int i = lo; i <= hi; i++){
+        for(int j = i-1; j >= lo; j--){
+            if(less(a[j+1], a[j]))
+                exch(a[j], a[j+1]);
+        }
+    }
+}
+
+#endif
 
 void merge(Item *a, Item *aux, int lo, int mid, int hi) {
     for(int k = lo; k <= hi; k++){
@@ -15,10 +29,20 @@ void merge(Item *a, Item *aux, int lo, int mid, int hi) {
 }
 
 void merge_sort(Item *a, Item *aux, int lo, int hi){
-    if(hi <= lo) return;
+    #if defined CUTOFF && (defined VERSAO_2 || defined VERSAO_4) 
+    if(hi <= lo + CUTOFF - 1){
+        insertion_sort(a, lo, hi);
+    #else
+    if(hi <= lo){
+    #endif
+        return;
+    }
     int mid = lo + ((hi - lo) / 2);
     merge_sort(a, aux, lo, mid);
     merge_sort(a, aux, mid+1, hi);
+    #if defined VERSAO_2 || defined VERSAO_4
+    if (!less(a[mid+1], a[mid])) return;
+    #endif
     merge(a, aux, lo, mid, hi);
 }
 
